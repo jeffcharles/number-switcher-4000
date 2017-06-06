34947b836fe1cfc5cfba9dc58392c913ec643eb1
Jeffrey Charles <jeffreycharles@gmail.com>
2017-06-05 21:24:27 -0400

    Add error messages when failing to login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,27 +6,45 @@ interface LoginProps {
   onSuccessfulLogin: () => void
 }
 
-export default class extends React.Component<LoginProps, {}> {
-  onSuccessfulGoogleLogin = (res: GoogleLoginResponse) => {
+interface LoginState {
+  errorMessage: string | null
+}
+
+export default class extends React.Component<LoginProps, LoginState> {
+  constructor() {
+    super();
+    this.state = { errorMessage: null };
+  }
+
+  onSuccessfulGoogleLogin = async (res: GoogleLoginResponse) => {
     AWS.config.credentials = new AWS.WebIdentityCredentials({
       RoleArn: 'arn:aws:iam::762636538502:role/number-switcher-4000',
       RoleSessionName: 'number-switcher-4000-web',
       WebIdentityToken: res.getAuthResponse().id_token
     });
-    this.props.onSuccessfulLogin();
+    const sts = new AWS.STS();
+    try {
+      await sts.getCallerIdentity().promise();
+      this.props.onSuccessfulLogin();
+    } catch (err) {
+      this.setState({ errorMessage: 'Account not permitted access' });
+    }
   }
 
   onFailedGoogleLogin = (err: any) => {
-    console.error(err);
+    this.setState({ errorMessage: err });
   }
 
   render() {
     return (
-      <GoogleLogin
-        clientId="721076834592-7iidl9sk4jfc09npct0c4ip8cnmtuknm.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={this.onSuccessfulGoogleLogin}
-        onFailure={this.onFailedGoogleLogin} />
+      <div>
+        <GoogleLogin
+          clientId="721076834592-7iidl9sk4jfc09npct0c4ip8cnmtuknm.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={this.onSuccessfulGoogleLogin}
+          onFailure={this.onFailedGoogleLogin} />
+        <p style={{color: 'red'}}>{this.state.errorMessage}</p>
+      </div>
     );
   }
 }
